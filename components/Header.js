4d38b51a02d78e7829bbb15e4ca9595a486689f1
simplyDonald <donaldabuah@gmail.com>
2023-01-03 16:09:24 -0500

    add date range package

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,12 @@
 
 import Image from "next/image"
 import { GlobeAltIcon, Bars3Icon, UserCircleIcon, UsersIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { useState } from "react"
 
 function Header() {
+  // track search input for location
+  const [searchInput, setSearchInput] = useState("")
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       <div className="relative flex items-center h-10 cursor-pointer">
@@ -15,7 +19,10 @@ function Header() {
       </div>
 
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-        <input className=" flex-grow bg-transparent outline-none pl-5 text-sm text-gray-600 placeholder-gray-400 " type="text" placeholder="Start your search" />
+        <input value={searchInput} onChange={(e)=> setSearchInput(e.target.value)} 
+        className=" flex-grow bg-transparent outline-none pl-5 text-sm text-gray-600 placeholder-gray-400 " type="text" 
+        placeholder="Start your search" />
+
         <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
 
@@ -25,10 +32,15 @@ function Header() {
         </p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
         <div className="flex space-x-2 items-center border-2 p-2 rounded-full ">
-         <Bars3Icon className="h-6" />
-         <UserCircleIcon className="h-6 cursor-pointer" />
+          <Bars3Icon className="h-6" />
+          <UserCircleIcon className="h-6 cursor-pointer" />
         </div>
       </div>
+      {searchInput && (
+        <div>
+          
+        </div>
+      ) }
     </header>
   )
 }
