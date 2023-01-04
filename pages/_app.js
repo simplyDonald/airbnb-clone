47740eb50f6c307b5a1c7d1f98d3b4187660dee4
simplyDonald <donaldabuah@gmail.com>
2023-01-04 15:46:08 -0500

    add bar package

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
 import '../styles/globals.css'
+import ProgressBar from '@badrap/bar-of-progress'
+
+// build the progress bar
+const progress = new ProgressBar({
+  size: 4,
+  color: '#FE595E',
+  className: 'z-50',
+  delay: 100,
+})
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
